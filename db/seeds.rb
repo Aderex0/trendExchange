d2878481b120e146408fe1a813f8679fa4574013
github username <github email address>
2019-11-14 10:37:25 +0000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-User.create(username: 'User1', password: 'abcd', account_balance: 0)
-
-OwnedTrend.create(name: 'Sonic', initial_tweet_volume: 200000, user_id: 1, initial_rank: 5, initial_valuation: 1.0, quantity_trends_purchased: 2)
-OwnedTrend.create(name: 'Frank Dobson', initial_tweet_volume: nil, user_id: 1, initial_rank: 2, initial_valuation: 1.0, quantity_trends_purchased: 2)
