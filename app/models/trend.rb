43d6857bb86d1838cf5a09bf77bba03d6ca2aebf
Aderex0 <54026981+Aderex0@users.noreply.github.com>
2019-12-10 16:14:01 +0000

    Update trend.rb

--- app/models/trend.rb
+++ app/models/trend.rb
@@ -5,10 +5,10 @@ class Trend < ApplicationRecord
     def self.fetch_trends
         
         client = Twitter::REST::Client.new do |config| 
-            config.consumer_key         = "FR1dPBikDl9G3BCD9VSC1lyEm"
-            config.consumer_secret      = "rUALnf9zDqOtFYTdBcBtmaa1rAAnhmuJgERcsjmo5MoOYL72A7"
-            config.access_token         = "4046571867-QMWcslJhGF21ZgbABtqs4f1E8O07fSOKbW11oap"
-            config.access_token_secret  = "xZLoJsjuboWLGdO8m8gB7U2q3JrBVtIcZdN36IfzMSnE8"
+            config.consumer_key         = "Key"
+            config.consumer_secret      = "Key"
+            config.access_token         = "Key"
+            config.access_token_secret  = "Key"
         end
     
         trends = client.trends(id = 23424975)
